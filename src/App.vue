bf01be60534a06751e39017449fb2309b59a8a48
Dede Ramdani <dede.ramdani@ns2solution.com>
2021-09-01 15:08:58 +0700

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld :msg="messages"/>
+  <!-- <Coba text="Cooook"/> -->
+  <input type="text" v-model="messages" />
+  <button @click="alertMessage">Alert</button>
+  <div>
+    <ul>
+      <li v-for="list in lists" v-bind:key="list">{{ list.title }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import _ from 'lodash'
+import HelloWorld from "./components/HelloWorld.vue";
+// import Coba from "./components/Coba.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    // Coba
+  },
+  
+  data(){
+    return {
+      messages: 'Hello World Cuuuuk!'
+    }
+  },
+
+  methods: {
+    alertMessage(){
+      alert(this.messages)
+    }
+  },
+
+  created() {
+    axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    .then(response => {
+      this.lists = _.slice(response.data,0,5)
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
   }
-}
+};
 </script>
 
 <style>
